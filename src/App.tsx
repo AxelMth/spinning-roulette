742956d67d2b1c9370bb7bae596a79bb67fdade1
Axel Mathieu <axel.m@padoa-group.com>
2021-07-17 22:10:56 +0200

    Generalized spin items

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,49 @@ function generateRandomSpin() {
   return Math.ceil(Math.random() * 50);
 }
 
+const RESTAURANTS = [
+  'Macdonald',
+  'Burger King',
+  'O\'Tacos',
+  'Okame',
+  'Apertivus',
+  'Shine Garden',
+  'Triumfo',
+  'Laouz',
+  'Petit Wagram',
+];
+
 function App() {
   const [spin, setSpin] = useState<number>(0);
-  const range: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <div className="container">
-      <div className="wheel-cursor-container">
-        <i className="fas fa-caret-down fa-3x"></i>
-      </div>
-      <div className="wheel-container">
-        <div className={`wheel spin-animation-${spin}`}>
-          { range.map((e: any): JSX.Element => (
-              <div id={"pieSlice" + e} className="hold">
-                <div className={"slice-name" + e}>
-                  <div className="slice-text">{ `test${e}` }</div>
+    <>
+      {RESTAURANTS.map((restaurant: any): JSX.Element => <div>{restaurant}</div>)}
+      <div className="container">
+        <div className="wheel-cursor-container">
+          <i className="fas fa-caret-down fa-3x"></i>
+        </div>
+        <div className="wheel-container">
+          <div className={`wheel spin-animation-${RESTAURANTS.length}-${spin}`}>
+            {RESTAURANTS.map((restaurant: any, index): JSX.Element => (
+              <div id={`pieSlice-${RESTAURANTS.length}-${index + 1}`} className="hold">
+                <div className={`slice-name-${RESTAURANTS.length}-${index + 1}`}>
+                  <div className="slice-text">{restaurant}</div>
                 </div>
-                <div className={`wheel-part ${(e % 2 === 0) ? "background-blue" : "background-red"}`}>
+                <div className={`wheel-part ${(index % 2 === 0) ? 'background-blue' : 'background-red'}`}>
                 </div>
               </div>
             ))
-          }
+            }
+          </div>
+        </div>
+        <div className="mt-3 is-centered">
+          <button className="button is-link is-rounded" onClick={() => {
+            setSpin(generateRandomSpin());
+          }}>Spin
+          </button>
         </div>
       </div>
-      <div className="mt-3 is-centered">
-        <button className="button is-link is-rounded" onClick={() => {
-          setSpin(generateRandomSpin());
-        }}>Spin</button>
-      </div>
-    </div>
+    </>
   );
 }
 
