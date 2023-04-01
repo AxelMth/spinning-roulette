12ed2fe4432a6e4897c1dd2649336cd13ed869e6
AxelMth <aujoulator@hotmail.fr>
2023-04-01 15:04:28 +0200

    adjusting filter margins

--- apps/frontend/src/components/FilterGroup.tsx
+++ apps/frontend/src/components/FilterGroup.tsx
@@ -10,20 +10,24 @@ interface Props {
 
 export const FilterGroup = ({filters, toggleFilterOption, reset}: Props) => {
   const [activeFilterName, setActiveFilterName] = useState<string | null>(null);
-  return <div className="p-1 is-flex is-justify-content-center" style={{flexWrap: 'wrap'}}>
-    {filters.map((f, filterIndex) => <Filter
-      key={filterIndex}
-      filter={f}
-      toggleFilterOption={toggleFilterOption}
-      isActive={activeFilterName === f.name}
-      toggleFilterMenu={() => {
-        if (activeFilterName === f.name) setActiveFilterName(null)
-        else setActiveFilterName(f.name)
-      }}
-      closeFilterMenu={() => {
-        setActiveFilterName(null)
-      }}
-    />)}
-    <button className={`button is-ghost`} onClick={() => {reset()}}>Réinitialiser</button>
-  </div>
+  return <>
+    <div className="mt-2 is-flex is-justify-content-center is-flex-wrap-wrap">
+      {filters.map((f, filterIndex) => <Filter
+        key={filterIndex}
+        filter={f}
+        toggleFilterOption={toggleFilterOption}
+        isActive={activeFilterName === f.name}
+        toggleFilterMenu={() => {
+          if (activeFilterName === f.name) setActiveFilterName(null)
+          else setActiveFilterName(f.name)
+        }}
+        closeFilterMenu={() => {
+          setActiveFilterName(null)
+        }}
+      />)}
+    </div>
+    <div className="is-flex is-justify-content-center is-align-content-center">
+      <button className={`button is-ghost`} onClick={() => {reset()}}>Réinitialiser</button>
+    </div>
+  </>
 };
